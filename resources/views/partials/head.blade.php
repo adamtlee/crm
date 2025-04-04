cd290848384fe25752a39fb2fd1efef7a9ce1890
adamtlee <adam.lee346@gmail.com>
2025-04-04 16:11:47 -0600

    removed favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,7 +3,10 @@
 
 <title>{{ $title ?? 'CRM' }}</title>
 
+<!-- Remove Laravel favicon -->
 <link rel="icon" href="data:,">
+<link rel="shortcut icon" href="data:,">
+<link rel="apple-touch-icon" href="data:,">
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
