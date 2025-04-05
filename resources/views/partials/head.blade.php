8fa1c54be46a57c03d5045ed565bb8a298ecfb7b
adamtlee <adam.lee346@gmail.com>
2025-04-05 14:58:39 -0600

    removed laravel favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,12 +1,12 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'CRM' }}</title>
+<title>{{ $title ?? 'BASIC' }}</title>
 
-<!-- Remove Laravel favicon -->
-<link rel="icon" href="data:,">
-<link rel="shortcut icon" href="data:,">
-<link rel="apple-touch-icon" href="data:,">
+<!-- Remove all favicons -->
+<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>">
+<link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>">
+<link rel="apple-touch-icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>">
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
